775f5dfb4068a6c2492a9897dcf5900679304467
marinagololobova <marinagololobova@mail.ru>
2023-11-08 16:58:42 +0300

    Attestation

---
+++ src/main/java/com/example/sockwarehouse/SockWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockWarehouseApplication.class, args);
+    }
+
+}
